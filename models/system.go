0d7102ab81f2bb3d00c1bad5ed2b1818553ab0dc
AdrianDuan <adrianduan@icloud.com>
2020-01-07 13:31:53 +0800

    update login history

--- models/system.go
+++ models/system.go
@@ -1,6 +1,9 @@
 package models
 
-import "github.com/jinzhu/gorm/dialects/postgres"
+import (
+	"github.com/jinzhu/gorm/dialects/postgres"
+	uuid "github.com/satori/go.uuid"
+)
 
 // JsError model
 type JsError struct {
@@ -9,6 +12,26 @@ type JsError struct {
 	Store postgres.Jsonb `json:"store" validate:"omitempty"`
 	Info  string         `json:"info" validate:"omitempty"`
 	URL   string         `json:"url" validate:"omitempty"`
+}
+
+// LoginHistory model
+type LoginHistory struct {
+	Base
+	User         User      `json:"user"`
+	UserID       uuid.UUID `json:"userID"`
+	Status       string    `json:"status"`
+	IP           string    `json:"ip"`
+	Country      string    `gorm:"DEFAULT:NULL" json:"country"`
+	CountryCode  string    `gorm:"DEFAULT:NULL" json:"countryCode"`
+	Region       string    `gorm:"DEFAULT:NULL" json:"region"`
+	RegionName   string    `gorm:"DEFAULT:NULL" json:"regionName"`
+	City         string    `gorm:"DEFAULT:NULL" json:"city"`
+	District     string    `gorm:"DEFAULT:NULL" json:"district"`
+	Longitude    float64   `gorm:"DEFAULT:NULL" json:"lon"`
+	Latitude     float64   `gorm:"DEFAULT:NULL" json:"lat"`
+	Timezone     string    `gorm:"DEFAULT:NULL" json:"timezone"`
+	ISP          string    `gorm:"DEFAULT:NULL" json:"isp"`
+	Organization string    `gorm:"DEFAULT:NULL" json:"org"`
 }
 
 // Info model
--- models/user.go
+++ models/user.go
@@ -1,7 +1,5 @@
 package models
 
-import uuid "github.com/satori/go.uuid"
-
 // User data model
 type User struct {
 	Base
@@ -11,24 +9,4 @@ type User struct {
 	UserType string `gorm:"DEFAULT:'user'" json:"userType" example:"admin"`                                                                                        // 用户角色
 	Password string `gorm:"DEFAULT:NULL" json:"-"`                                                                                                                 // 密码
 	State    string `gorm:"DEFAULT:'inactive'" json:"state" example:"active"`                                                                                      // 状态
-}
-
-// LoginHistory model
-type LoginHistory struct {
-	Base
-	User         User      `json:"user"`
-	UserID       uuid.UUID `json:"userID"`
-	Status       string    `json:"status"`
-	IP           string    `json:"ip"`
-	Country      string    `gorm:"DEFAULT:NULL" json:"country"`
-	CountryCode  string    `gorm:"DEFAULT:NULL" json:"countryCode"`
-	Region       string    `gorm:"DEFAULT:NULL" json:"region"`
-	RegionName   string    `gorm:"DEFAULT:NULL" json:"regionName"`
-	City         string    `gorm:"DEFAULT:NULL" json:"city"`
-	District     string    `gorm:"DEFAULT:NULL" json:"district"`
-	Longitude    float64   `gorm:"DEFAULT:NULL" json:"lon"`
-	Latitude     float64   `gorm:"DEFAULT:NULL" json:"lat"`
-	Timezone     string    `gorm:"DEFAULT:NULL" json:"timezone"`
-	ISP          string    `gorm:"DEFAULT:NULL" json:"isp"`
-	Organization string    `gorm:"DEFAULT:NULL" json:"org"`
 }
